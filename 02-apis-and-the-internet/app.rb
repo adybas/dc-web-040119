7c839fc2ae80370f299646b0305332ed02068ed8
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-04-02 12:15:21 -0400

    post lecture 02

--- 02-apis-and-the-internet/app.rb
+++ 02-apis-and-the-internet/app.rb
@@ -4,3 +4,72 @@ require 'pry'
 
 GOOGLE_BOOKS_API_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
 
+
+def welcome_user
+    # welcome the user 
+    puts "Welcome user!  This is my book searcher!\n"
+end
+
+def get_search_term
+    # get a search term
+    puts "What would you like to search for?"
+    search_term = gets.chomp
+    return search_term
+end
+
+def get_book_info(search_term)
+    # make a request to api to get book info
+    url = GOOGLE_BOOKS_API_BASE_URL + search_term # todo:  handle spaces in search term
+    response = RestClient.get(url)
+    return response
+end
+
+def parse_book_info(response)
+    # parse the book info
+    # take each book and get the author, title, and description
+    json = JSON.parse(response)
+    json["items"].each do |book_info|
+        title = book_info["volumeInfo"]["title"]
+        author = parse_authors(book_info["volumeInfo"]["authors"]) 
+        description = parse_description(book_info["volumeInfo"]["description"])
+
+        show_book_info(title, author, description)
+    end
+end
+
+def parse_authors(authors)
+    #["Joe Johnson", "Mary Maryson"] >> "Joe Johnson & Mary Maryson"
+    # if authors
+    #     return authors.join(" & ")
+    # else
+    #     return "Author Unknown"
+    # end
+    authors ? authors.join(" & ") : "Author Unknown"
+end
+
+def parse_description(description)
+    if description
+        description = description[0..100] + "..."
+    else 
+        description = "We have no idea what this book is about."
+    end
+end
+
+def show_book_info(title, author, description)
+    puts "#{title} by #{author}: #{description}"
+    puts "*************\n"
+end
+
+# display the book info
+
+def main
+    welcome_user
+    search_term = get_search_term
+    response = get_book_info(search_term)
+    parse_book_info(response)
+
+end
+
+main()
+# binding.pry
+# "hi"
--- 02-apis-and-the-internet/donow.rb
+++ 02-apis-and-the-internet/donow.rb
@@ -1,4 +1,3 @@
-
 # For each of the following examples, write two methods that solve the 
 # problem:  One that uses `.each` and one that uses one of the higher level 
 # iterator methods
@@ -17,20 +16,38 @@
 nums = (1..20).to_a #creates an array of all numbers from 1-20
 
 # 1.  Create an array of numbers where each new number is three times as big as
-# its original number (e.g., [3, 6, 9])
+# its original number (e.g., [3, 6, 9, ...])
 
+# three_times = []
+# nums.each do |number|
+#     three_times.push(number * 3)
+# end
+# print three_times
+
+# three_times = nums.map do |item|
+#     item * 3
+# end
+
+# print three_times
 
 # 2.  Find the first number that is divisible by 7
 
+# puts nums.find {|num| num % 7 == 0}
 
 # 3.  Find all numbers that are divisible by 7
 
+# print nums.select {|num| num % 7 == 0}
 
 # 4.  Find the first number that is divisible by 7 AND greater than 10
+
+# print nums.find {|num| num % 7 == 0 && num > 10}
 
 
 # Bonus:
 
 
-# 5.  Create an array of the squares (the number times itself) of all numbers
+# 5.  Create an array of the squares (the number times itself) of all numbers (in the range, Ann)
 # that are divisible by 7
+
+squares = nums.select {|num| num % 7 == 0}.map {|num| num ** 2}
+print squares
