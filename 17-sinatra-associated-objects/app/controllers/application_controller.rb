e4eba41f941a28aa897c8320a5715e8c76bbb56b
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-04-24 09:34:28 -0400

    prelecture 17

---
+++ 17-sinatra-associated-objects/app/controllers/application_controller.rb
@@ -0,0 +1,55 @@
+require "pry"
+
+class ApplicationController < Sinatra::Base
+
+    set :views, "app/views"
+    set :method_override, true
+
+    get "/" do 
+        erb :home
+    end
+
+    get "/books" do
+        @books = Book.all
+        erb :index
+    end
+
+    get "/books/new" do 
+        erb :new
+    end
+
+    get "/books/:id" do 
+        id = params[:id]
+        @book = Book.find(id)
+        erb :show
+    end
+
+    post "/books" do
+        author = params[:author]
+        title = params[:title]
+        snippet = params[:snippet]
+        @book = Book.create(author: author, title: title, snippet: snippet)
+        redirect "/books/#{book.id}"
+    end
+
+    get "/books/:id/edit" do
+        @book = Book.find(params[:id])
+        erb :edit
+    end
+
+    patch "/books/:id" do
+        @book = Book.find(params[:id])
+        @book.update(title: params[:title],
+                    author: params[:author],
+                    snippet: params[:snippet])
+        redirect "/books/#{@book.id}"
+    end
+
+    delete "/books/:id" do
+        # binding.pry
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect "/books"
+    end
+
+end
