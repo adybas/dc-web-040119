00edb2bddea68a11f552a27474086eb9f60603e5
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-04-29 16:33:46 -0400

    post lecture 18 pt 2

--- 18-rails-crud/snack-tracker/config/routes.rb
+++ 18-rails-crud/snack-tracker/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :snacks, only: [:index, :show]
+  resources :snacks
   get('/favorites', {to: 'snacks#favorites', as: 'favorites'})
 end
